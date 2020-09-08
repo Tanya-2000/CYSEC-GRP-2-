9b48a6896ad547f67ca49567bacace25ca0bc58f
Tanya-2000 <68813266+Tanya-2000@users.noreply.github.com>
2020-09-08 16:28:17 +0530

    QUES-1

---
+++ PYTHON ASSIGNMENT 2/pangram.py
@@ -0,0 +1,10 @@
+import string, sys
+
+
+def ispangram(str1, alphabet=string.ascii_lowercase):
+    alphaset = set(alphabet)
+    return alphaset <= set(str1.lower())
+
+
+print(ispangram('ababa'))
+
